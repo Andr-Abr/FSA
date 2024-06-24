b40d840469a2f4344c57926ec6cca6dac3d5cb69
Andr-Abr <1218236@gmail.com>
2024-06-24 01:30:24 -0500

    First commit

---
+++ fsaplant/eventos.js
@@ -0,0 +1,33 @@
+// Validación de formularios
+        (function () {
+            'use strict'
+            var forms = document.querySelectorAll('.needs-validation')
+
+            Array.prototype.slice.call(forms)
+                .forEach(function (form) {
+                    form.addEventListener('submit', function (event) {
+                        if (!form.checkValidity()) {
+                            event.preventDefault()
+                            event.stopPropagation()
+                        }
+                        form.classList.add('was-validated')
+                    }, false)
+                })
+        })();
+
+        document.addEventListener('DOMContentLoaded', function () {
+            // modales
+            var registerModal = new bootstrap.Modal(document.getElementById('registerModal'), {
+                keyboard: false
+            });
+        
+            var loginModal = new bootstrap.Modal(document.getElementById('loginModal'), {
+                keyboard: false
+            });
+        
+            // popovers
+            var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'));
+            var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+                return new bootstrap.Popover(popoverTriggerEl);
+            });
+        });
